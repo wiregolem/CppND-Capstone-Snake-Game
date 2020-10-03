7f531a8940b4068a690af50b2c4774250ceb2b61
wiregolem <friedl.kevin@gmail.com>
2020-10-02 23:55:21 -0500

    Added TextureLoader definition

---
+++ src/TextureLoader.cpp
@@ -0,0 +1,16 @@
+#include <TextureLoader.h>
+
+SDL_Texture* TextureLoader::LoadTexture(const char* filename, SDL_Renderer* renderer)
+{
+ /*Load image from file as an SDL Surface*/
+ SDL_Surface* temporary = IMG_Load(filename);
+
+ /*Convert surface into a texture to be rendered*/
+ SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, temporary);
+
+ /*Free unnecessary surface resource*/
+ SDL_FreeSurface(tempSurface)
+
+ /*return texture pointer for use*/
+ return texture;
+}
